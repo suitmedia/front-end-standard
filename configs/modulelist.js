8ec64cd3102d43186633fa0156c414d91dcb72ac
Andi Ersaldy Raisha <andi.ersaldy@gmail.com>
2017-07-10 11:40:02 -0700

    Fix undetected folder by github by removing '_' prefix from modules and sections folder

--- configs/modulelist.js
+++ configs/modulelist.js
@@ -1,182 +1,182 @@
 var moduleList = [
         {
             'name': 'CSS Declaration',
-            'url': '_modules/css1.html',
+            'url': 'modules/css1.html',
             'section': 2
         },
         {
             'name': 'Avoid deep nesting',
-            'url': '_modules/css2.html',
+            'url': 'modules/css2.html',
             'section': 2
         },
         {
             'name': 'Z-Index',
-            'url': '_modules/css3.html',
+            'url': 'modules/css3.html',
             'section': 2
         },
         {
             'name': 'Spacing and new lines',
-            'url': '_modules/css4.html',
+            'url': 'modules/css4.html',
             'section': 2
         },
         {
             'name': 'CSS Prefixing',
-            'url': '_modules/css5.html',
+            'url': 'modules/css5.html',
             'section': 2
         },
         {
             'name': 'No ID and !important in CSS',
-            'url': '_modules/css6.html',
+            'url': 'modules/css6.html',
             'section': 2
         },
         {
             'name': 'Avoid contextual styles',
-            'url': '_modules/css7.html',
+            'url': 'modules/css7.html',
             'section': 2
         },
         {
             'name': 'Don`t change base style',
-            'url': '_modules/css8.html',
+            'url': 'modules/css8.html',
             'section': 2
         },
         {
             'name': 'Use class to style, avoid using tag.',
-            'url': '_modules/css9.html',
+            'url': 'modules/css9.html',
             'section': 2
         },
         {
             'name': 'Don`t use BEM SCSS shortcut',
-            'url': '_modules/css10.html',
+            'url': 'modules/css10.html',
             'section': 2
         },
         {
             'name': 'Don`t use CSS @import',
-            'url': '_modules/css11.html',
+            'url': 'modules/css11.html',
             'section': 2
         },
         {
             'name': 'Use transform and opacity for high performance animation',
-            'url': '_modules/css12.html',
+            'url': 'modules/css12.html',
             'section': 2
         },
         {
             'name': 'Standard HTML Declaration',
-            'url': '_modules/html1.html',
+            'url': 'modules/html1.html',
             'section': 1
         },
         {
             'name': 'Penamaan class dan ID',
-            'url': '_modules/html2.html',
+            'url': 'modules/html2.html',
             'section': 1
         },
         {
             'name': 'Class naming convention',
-            'url': '_modules/html3.html',
+            'url': 'modules/html3.html',
             'section': 1
         },
         {
             'name': 'Penggunaan tag <a> dan <button>',
-            'url': '_modules/html4.html',
+            'url': 'modules/html4.html',
             'section': 1
         },
         {
             'name': 'Label untuk tiap input',
-            'url': '_modules/html5.html',
+            'url': 'modules/html5.html',
             'section': 1
         },
         {
             'name': 'Slick Carousel',
-            'url': '_modules/tools1.html',
+            'url': 'modules/tools1.html',
             'section': 4
         },
         {
             'name': 'Perfect Tooltip',
-            'url': '_modules/tools2.html',
+            'url': 'modules/tools2.html',
             'section': 4
         },
         {
             'name': 'Baguette Box',
-            'url': '_modules/tools3.html',
+            'url': 'modules/tools3.html',
             'section': 4
         },
         {
             'name': 'Baze Modal',
-            'url': '_modules/tools4.html',
+            'url': 'modules/tools4.html',
             'section': 4
         },
         {
             'name': 'Baze Validation',
-            'url': '_modules/tools5.html',
+            'url': 'modules/tools5.html',
             'section': 4
         },
         {
             'name': 'Code structure',
-            'url': '_modules/js1.html',
+            'url': 'modules/js1.html',
             'section': 3
         },
         {
             'name': 'Gunakan literal expression',
-            'url': '_modules/js2.html',
+            'url': 'modules/js2.html',
             'section': 3
         },
         {
             'name': 'Prefix $',
-            'url': '_modules/js3.html',
+            'url': 'modules/js3.html',
             'section': 3
         },
         {
             'name': 'Cache selector dan string.',
-            'url': '_modules/js4.html',
+            'url': 'modules/js4.html',
             'section': 3
         },
         {
             'name': 'Simpan string yang dibutuhkan dalam Object',
-            'url': '_modules/js5.html',
+            'url': 'modules/js5.html',
             'section': 3
         },
         {
             'name': 'Gunakan strict equality ( === )',
-            'url': '_modules/js6.html',
+            'url': 'modules/js6.html',
             'section': 3
         },
         {
             'name': 'Gunakan function expression',
-            'url': '_modules/js7.html',
+            'url': 'modules/js7.html',
             'section': 3
         },
         {
             'name': 'Hindari anonymous function',
-            'url': '_modules/js8.html',
+            'url': 'modules/js8.html',
             'section': 3
         },
         {
             'name': 'Unbind event handler sebelum menambahkan event handler.',
-            'url': '_modules/js9.html',
+            'url': 'modules/js9.html',
             'section': 3
         },
         {
             'name': 'Return early',
-            'url': '_modules/js10.html',
+            'url': 'modules/js10.html',
             'section': 3
         },
         {
             'name': 'Convert String to Int',
-            'url': '_modules/js11.html',
+            'url': 'modules/js11.html',
             'section': 3
         },
         {
             'name': 'Loop',
-            'url': '_modules/js12.html',
+            'url': 'modules/js12.html',
             'section': 3
         },
         {
             'name': 'Console.*',
-            'url': '_modules/js13.html',
+            'url': 'modules/js13.html',
             'section': 3
         },
         {
             'name': 'JSHint',
-            'url': '_modules/js14.html',
+            'url': 'modules/js14.html',
             'section': 3
         }
     ];
