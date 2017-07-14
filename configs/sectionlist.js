ae7e714182ddd16edd886cecc783cc8e7f165e26
Andi Ersaldy Raisha <andi.ersaldy@gmail.com>
2017-07-14 14:54:14 +0700

    Change ' to " on configs

--- configs/sectionlist.js
+++ configs/sectionlist.js
@@ -1,26 +1,26 @@
 var sectionList = [
         {
-            'sectionId' : 'html',
-            'sectionName' : 'HTML Guideline',
-            'sectionHeader' : 'HTML',
-            'sectionIcon' : '<i class="fa fa-code"></i>'
+            "sectionId" : "html",
+            "sectionName" : "HTML Guideline",
+            "sectionHeader" : "HTML",
+            "sectionIcon" : "<i class=\"fa fa-code\"></i>"
         },
         {
-            'sectionId' : 'css',
-            'sectionName' : 'CSS/SASS Guideline',
-            'sectionHeader' : 'CSS/SASS',
-            'sectionIcon' : 'CSS'
+            "sectionId" : "css",
+            "sectionName" : "CSS/SASS Guideline",
+            "sectionHeader" : "CSS/SASS",
+            "sectionIcon" : "CSS"
         },
         {
-            'sectionId' : 'js',
-            'sectionName' : 'JavaScript Guideline',
-            'sectionHeader' : 'JavaScript',
-            'sectionIcon' : 'JS'
+            "sectionId" : "js",
+            "sectionName" : "JavaScript Guideline",
+            "sectionHeader" : "JavaScript",
+            "sectionIcon" : "JS"
         },
         {
-            'sectionId' : 'tools',
-            'sectionName' : 'Tools & Plugin',
-            'sectionHeader' : 'Tools & Plugin',
-            'sectionIcon' : '<i class="fa fa-wrench"></i>'
+            "sectionId" : "tools",
+            "sectionName" : "Tools & Plugin",
+            "sectionHeader" : "Tools & Plugin",
+            "sectionIcon" : "<i class=\"fa fa-wrench\"></i>"
         }
     ];
